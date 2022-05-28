8d2cc07848a044b89a682a22cc5cb0ca3bf7d39c
Jeremy Maia <jmaia@student.42.fr>
2022-05-28 15:59:38 +0200

    Add scopes to main

--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 12:15:59 by jmaia             #+#    #+#             */
-/*   Updated: 2022/05/28 15:43:58 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/05/28 15:58:59 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,47 +17,52 @@
 
 int	main(void)
 {
-	ClapTrap	cTrap("Alice");
+	{
+		ClapTrap	cTrap("Alice");
 
-	std::cout << cTrap << std::endl;
+		std::cout << cTrap << std::endl;
 
-	cTrap.attack("Bob");
-	cTrap.attack("J");
-	cTrap.attack("");
+		cTrap.attack("Bob");
+		cTrap.attack("J");
+		cTrap.attack("");
 
-	std::cout << cTrap << std::endl;
+		std::cout << cTrap << std::endl;
 
-	cTrap.takeDamage(3);
-	cTrap.takeDamage(1);
-	cTrap.takeDamage(0);
+		cTrap.takeDamage(3);
+		cTrap.takeDamage(1);
+		cTrap.takeDamage(0);
 
-	std::cout << cTrap << std::endl;
+		std::cout << cTrap << std::endl;
 
-	cTrap.beRepaired(12);
-	cTrap.beRepaired(0);
+		cTrap.beRepaired(12);
+		cTrap.beRepaired(0);
 
-	std::cout << cTrap << std::endl;
+		std::cout << cTrap << std::endl;
 
-	cTrap.beRepaired(-2);
-	cTrap.takeDamage(-2);
-	cTrap.beRepaired(1);
-	cTrap.beRepaired(1);
-	cTrap.beRepaired(1);
-	cTrap.beRepaired(1);
-	std::cout << cTrap << std::endl;
-	cTrap.beRepaired(1);
-	cTrap.beRepaired(1);
-	cTrap.attack("John");
+		cTrap.beRepaired(-2);
+		cTrap.takeDamage(-2);
+		cTrap.beRepaired(1);
+		cTrap.beRepaired(1);
+		cTrap.beRepaired(1);
+		cTrap.beRepaired(1);
+		std::cout << cTrap << std::endl;
+		cTrap.beRepaired(1);
+		cTrap.beRepaired(1);
+		cTrap.attack("John");
 
-	cTrap = ClapTrap("Carol");
-	cTrap.takeDamage(100);
-	cTrap.attack("A");
-	cTrap.beRepaired(1);
+		cTrap = ClapTrap("Carol");
+		cTrap.takeDamage(100);
+		cTrap.attack("A");
+		cTrap.beRepaired(1);
+	}
 
 	std::cout << "### ScavTrap ###" << std::endl;
 
-	ScavTrap	sTrap("Dave");
+	{
+		ScavTrap	sTrap("Dave");
 
-	std::cout << sTrap << std::endl;
-	sTrap.guardGuate();
+		std::cout << sTrap << std::endl;
+		sTrap.attack("Eve");
+		sTrap.guardGuate();
+	}
 }
